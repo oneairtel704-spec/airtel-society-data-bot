fedaaeecdb66b8d319bf3257b0e532cc24332267
Airtel Interface <oneairtel704@gmail.com>
2026-05-17 20:51:03 +0530

    fixed print message

--- bot.py
+++ bot.py
@@ -22,11 +22,11 @@ def search_buildings(rsu, building_name):
     cursor = conn.cursor()
     query = (
         "SELECT building_name, building_type, locality, total_units,"
-        " airtel_postpaid_users, airtel_prepaid_users, airtel_wifi_users, airtel_oap_users"
-        " FROM buildings"
-        " WHERE rsu = %s"
-        " AND building_name ILIKE %s"
-        " LIMIT 5"
+        "airtel_postpaid_users, airtel_prepaid_users, airtel_wifi_users, airtel_oap_users"
+        "FROM buildings"
+        "WHERE rsu = %s"
+        "AND building_name ILIKE %s"
+        "LIMIT 5"
     )
     cursor.execute(query, (rsu.upper(), f"%{building_name}%"))
     results = cursor.fetchall()
@@ -83,7 +83,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         prepaid = row[5]
         wifi = row[6]
         oap = row[7]
-        print("Values: " + str(total) + " " + str(postpaid) + " " + str(prepaid + " " + str(wifi) + " " + str(oap)))
+        print("Values: " + str(total) + " " + str(postpaid) + " " + str(prepaid) + " " + str(wifi) + " " + str(oap))
 
         def fmt(val, total):
          if val and total > 0:
